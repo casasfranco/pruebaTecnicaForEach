ffd342c86d63f71742e4d685c06567361a8e9bae
Franco Casas <casassfranco@gmail.com>
2020-10-06 19:58:54 -0300

    Se creo la rama dev. Estructura del proyecto. Se instalo react-router-dom. Se comenzo a crear algunos componentes compartidos. Si agrego bootstrap al proyecto.

---
+++ src/components/common/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-info text-light">
+      <div className="container pt-3 text-center">
+        <p>Todos los derechos reservados &copy; 2020</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
